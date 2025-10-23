30cbd1b4382df5e73248e2ae1c3c026deb506a14
mykzeman <mykalmayne@gmail.com>
2025-10-23 17:41:15 +1300

    q

--- src/supabase.jsx
+++ src/supabase.jsx
@@ -4,7 +4,11 @@ import { createClient } from '@supabase/supabase-js';
 
 
 
-const supabase = createClient( import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
+ 
         
